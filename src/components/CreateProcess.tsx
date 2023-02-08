5286f0a700e56692edfbc35ef10cbff886e524f9
Gerard <g.roura.v@protonmail.com>
2023-02-08 09:05:55 +0000

    useFormik, FormikProvider

--- src/components/CreateProcess.tsx
+++ src/components/CreateProcess.tsx
@@ -6,56 +6,55 @@ import {
   Text,
   VStack,
 } from '@chakra-ui/react';
-import { Field, Formik } from 'formik';
+import { Field, Form, FormikProvider, useFormik } from 'formik';
 import Addresses from './Addresses';
 import Questions from './Questions';
 
+const INITIAL_VALUES = {
+  title: '',
+  description: '',
+  addresses: [{ address: '', weight: 0 }],
+  weightedVote: {
+    active: false,
+    total: 0,
+  },
+  questions: [
+    {
+      title: '',
+      description: '',
+      options: ['', ''],
+    },
+  ],
+};
+
 const CreateProcess = () => {
+  const formik = useFormik({
+    initialValues: INITIAL_VALUES,
+    onSubmit: (values) => console.log(values),
+  });
+
   return (
-    <Formik
-      initialValues={{
-        title: '',
-        description: '',
-        addresses: [{ address: '', weight: 0 }],
-        weightedVote: {
-          active: false,
-          total: 0,
-        },
-        questions: [
-          {
-            title: '',
-            description: '',
-            options: ['', ''],
-          },
-        ],
-      }}
-      onSubmit={(values) => console.log(values)}
-    >
-      {({ values }) => {
-        console.log(values);
-        return (
-          <form>
-            <VStack as="fieldset" spacing={4} align="flex-start">
-              <Text as="legend">Form</Text>
-              <FormControl>
-                <FormLabel htmlFor="weighted">Weighted vote</FormLabel>
-                <Field as={Checkbox} id="weighted" name="weightedVote.active" />
-              </FormControl>
-              <FormControl>
-                <FormLabel htmlFor="title">Title</FormLabel>
-                <Field as={Input} id="title" name="title" />
-              </FormControl>
-              <FormControl>
-                <FormLabel htmlFor="description">Description</FormLabel>
-                <Field as={Input} id="description" name="description" />
-              </FormControl>
-              <Addresses />
-              <Questions />
-            </VStack>
-          </form>
-        );
-      }}
-    </Formik>
+    <FormikProvider value={formik}>
+      <Form>
+        <VStack as="fieldset" spacing={4} align="flex-start">
+          <Text as="legend">Form</Text>
+          <FormControl>
+            <FormLabel htmlFor="weighted">Weighted vote</FormLabel>
+            <Field as={Checkbox} id="weighted" name="weightedVote.active" />
+          </FormControl>
+          <FormControl>
+            <FormLabel htmlFor="title">Title</FormLabel>
+            <Field as={Input} id="title" name="title" />
+          </FormControl>
+          <FormControl>
+            <FormLabel htmlFor="description">Description</FormLabel>
+            <Field as={Input} id="description" name="description" />
+          </FormControl>
+          <Addresses />
+          <Questions />
+        </VStack>
+      </Form>
+    </FormikProvider>
   );
 };
 
